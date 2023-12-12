7cd4e8022745674ba8a881ccd7b64a9823194494
Abhinandengholi <Abhinand.engholi@ust.com>
2023-12-12 13:05:46 +0530

    12/12/23

--- RestEx/Program.cs
+++ RestEx/Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestEx;
 using RestSharp;
 
 
@@ -66,72 +68,101 @@ var client=new RestClient(baseurl);
 //Console.WriteLine(deleteUserResponse.Content);
 
 
-GetAllUsers(client);
-GetUser(client);
-CreateUser(client);
-UpdateUser(client);
-DeleteUser(client);
-//Get all
-static void GetAllUsers(RestClient client)
-{
-    var getUserRequest = new RestRequest("users", Method.Get);
-   
-    var getUserResponse = client.Execute(getUserRequest);
-    Console.WriteLine(getUserResponse.Content);
-}
-//Get User
-static void GetUser(RestClient client)
-{
-    var getUserRequest = new RestRequest("users", Method.Get);
-    getUserRequest.AddQueryParameter("page", "1");//Adding query parameter
-    var getUserResponse = client.Execute(getUserRequest);
-    if (getUserResponse.StatusCode == System.Net.HttpStatusCode.OK)
-    { //Parse JSON response content
-        JObject? userJson = JObject.Parse(getUserResponse?.Content);
-        string? pageno = userJson["page"]?.ToString();
+//GetAllUsers(client);
+//GetUser(client);
+//CreateUser(client);
+//UpdateUser(client);
+//DeleteUser(client);
+////Get all
+//static void GetAllUsers(RestClient client)
+//{
+//    var getUserRequest = new RestRequest("users", Method.Get);
 
-        string? userName = userJson["data"]?["first_name"]?.ToString();
-        string? userLastName = userJson["data"]?["last_name"]?.ToString();
-        Console.WriteLine($"User Name:{pageno}{userName}{userLastName}");
-    }
-    else
-    {
-        Console.WriteLine($"Error:{getUserResponse.ErrorMessage}");
-    }
+//    var getUserResponse = client.Execute(getUserRequest);
+//    Console.WriteLine(getUserResponse.Content);
+//}
+////Get User
+//static void GetUser(RestClient client)
+//{
+//    var getUserRequest = new RestRequest("users", Method.Get);
+//    getUserRequest.AddQueryParameter("page", "1");//Adding query parameter
+//    var getUserResponse = client.Execute(getUserRequest);
+//    if (getUserResponse.StatusCode == System.Net.HttpStatusCode.OK)
+//    { //Parse JSON response content
+//        JObject? userJson = JObject.Parse(getUserResponse?.Content);
+//        string? pageno = userJson["page"]?.ToString();
+
+//        string? userName = userJson["data"]?["first_name"]?.ToString();
+//        string? userLastName = userJson["data"]?["last_name"]?.ToString();
+//        Console.WriteLine($"User Name:{pageno}{userName}{userLastName}");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Error:{getUserResponse.ErrorMessage}");
+//    }
 
 
-}
- //POST or create
- static void CreateUser(RestClient client)
-{
-    var createUserRequest = new RestRequest("users", Method.Post);
-    createUserRequest.AddHeader("Content-Type", "application/json");
-    createUserRequest.AddJsonBody(new { name = "John wick", job = "Software Developer" });
-    var createUserResponse = client.Execute(createUserRequest);
-    Console.WriteLine("POST Create User response");
-    Console.WriteLine(createUserResponse.Content);
-}
+//}
+// //POST or create
+// static void CreateUser(RestClient client)
+//{
+//    var createUserRequest = new RestRequest("users", Method.Post);
+//    createUserRequest.AddHeader("Content-Type", "application/json");
+//    createUserRequest.AddJsonBody(new { name = "John wick", job = "Software Developer" });
+//    var createUserResponse = client.Execute(createUserRequest);
+//    Console.WriteLine("POST Create User response");
+//    Console.WriteLine(createUserResponse.Content);
+//}
 
-//PUT OR PATCH or update
-static void UpdateUser (RestClient client)
-{ 
-var updateUserRequest = new RestRequest("users/2", Method.Put);
-updateUserRequest.AddQueryParameter("data.id", "2");//Adding query parameter
-updateUserRequest.AddHeader("Content-Type", "application/json");
-updateUserRequest.AddJsonBody(new { name = "John wick", job = "Software Developer" });
-var updateUserResponse = client.Execute(updateUserRequest);
-Console.WriteLine("PUT update User response" + updateUserResponse.Content);
-}
+////PUT OR PATCH or update
+//static void UpdateUser (RestClient client)
+//{ 
+//var updateUserRequest = new RestRequest("users/2", Method.Put);
+//updateUserRequest.AddQueryParameter("data.id", "2");//Adding query parameter
+//updateUserRequest.AddHeader("Content-Type", "application/json");
+//updateUserRequest.AddJsonBody(new { name = "John wick", job = "Software Developer" });
+//var updateUserResponse = client.Execute(updateUserRequest);
+//Console.WriteLine("PUT update User response" + updateUserResponse.Content);
+//}
 
-//DELETE
-static void DeleteUser(RestClient client)
-{
-    var deleteUserRequest = new RestRequest("users/2", Method.Delete);
-    var deleteUserResponse = client.Execute(deleteUserRequest);
-    Console.WriteLine("DELETE User response");
-    Console.WriteLine(deleteUserResponse.Content);
+////DELETE
+//static void DeleteUser(RestClient client)
+//{
+//    var deleteUserRequest = new RestRequest("users/2", Method.Delete);
+//    var deleteUserResponse = client.Execute(deleteUserRequest);
+//    Console.WriteLine("DELETE User response");
+//    Console.WriteLine(deleteUserResponse.Content);
 
-}
+//}
+//GetSingleUser(client);
+//static void GetSingleUser(RestClient client)
+//{
+//    var getUserRequest = new RestRequest("users/2", Method.Get);
+//    var getUserResponse = client.Execute(getUserRequest);
+
+//    if (getUserResponse.StatusCode == System.Net.HttpStatusCode.OK)
+//    { 
+//        //deserialize JSON reponse content into C# object
+//        var response=JsonConvert.DeserializeObject<UserDataResponse>(getUserResponse.Content);
+        
+//        UserData? user=response?.Data;
+
+//        //access properties of the deserilized object
+//        Console.WriteLine($"User ID:{user?.Id}");
+//        Console.WriteLine($"User Email:{user?.Email}");
+//        Console.WriteLine($"User Name:{user?.FirstName}{user?.LastName}");
+//        Console.WriteLine($"User Avatar:{user?.Avatar}");
+
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Error:{getUserResponse.ErrorMessage}");
+//    }
+
+
+//}
+APIWithExcption apt=new APIWithExcption();
+apt.GetSingleUser();
 
 
 
---
+++ RestExcepNunit/ReqResAPITests.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestExcepNunit
+{
+    [TestFixture]
+    internal class ReqResAPITests
+    {
+        private RestClient client;
+        private string baseUrl = "https://reqres.in/api/";
+        [SetUp]
+        public void SetUp()
+        {
+            client = new RestClient(baseUrl);
+        }
+        [Test]
+        [Order(1)]
+        public void GetSingleUser()
+        {
+
+            var req = new RestRequest("users/2", Method.Get);
+            var response = client.Execute(req);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            var userdata = JsonConvert.DeserializeObject<UserDataResponse>(response.Content);
+            UserData? user = userdata?.Data;
+            Assert.NotNull(user);
+            Assert.That(user.Id, Is.EqualTo(2));
+            Assert.IsNotEmpty(user.Email);
+
+        }
+        [Test, Order(2)]
+        public void CreateUser()
+        {
+            var createUserRequest = new RestRequest("users", Method.Post);
+            createUserRequest.AddHeader("Content-Type", "application/json");
+            createUserRequest.AddJsonBody(new { name = "John wick", job = "Software Developer" });
+            var createUserResponse = client.Execute(createUserRequest);
+            Assert.That(createUserResponse.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+            var user = JsonConvert.DeserializeObject<UserData>(createUserResponse.Content);
+            Assert.NotNull(user);
+            //Assert.IsNotEmpty(user.Email);
+            Console.WriteLine(createUserResponse.Content);
+        }
+        [Test, Order(3)]
+        public void UpdateUser()
+        {
+            {
+                var updateuserrequest = new RestRequest("users/2", Method.Put);
+                updateuserrequest.AddHeader("content-type", "application/json");
+                updateuserrequest.AddJsonBody(new { name = "john wick", job = "software developer" });
+                var updateUserResponse = client.Execute(updateuserrequest);
+                Assert.That(updateUserResponse.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+                var user = JsonConvert.DeserializeObject<UserData>(updateUserResponse.Content);
+                Assert.NotNull(user);
+                Console.WriteLine(updateUserResponse.Content);
+            }
+        }
+        [Test, Order(4)]
+        public void DeleteUser()
+        {
+            {
+                var deleteUserRequest = new RestRequest("users/2", Method.Delete);
+                var deleteUserResponse = client.Execute(deleteUserRequest);
+                Assert.That(deleteUserResponse.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.NoContent));
+            }
+        }
+        [Test, Order(5)]
+        public void GetNonExistingUser()
+        {
+            var request = new RestRequest("users/999", Method.Get);
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.NotFound));
+        }
+    }
+}
